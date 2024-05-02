57c26a19d791306b71228d19975a2324e6a591c3
Arfan Hidayat Priyantono <arfan.hidayat.priyantono@gmail.com>
2024-05-02 11:24:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,18 @@ def fetch_stock_data(symbols):
     for symbol in symbols:
         stock = yf.Ticker(symbol)
         info = stock.info
+        current_price = info.get('regularMarketPrice')
+        if current_price is None:
+            current_price = info.get('regularMarketPreviousClose')  # Gunakan harga tutup sebelumnya jika harga saat ini tidak tersedia
         stock_data = {
+            'Symbol': symbol,
             'Price/Book (PBVR)': info.get('priceToBook'),
             'Trailing P/E (PER)': info.get('trailingPE'),
             'Total Debt/Equity (mrq) (DER)': info.get('debtToEquity'),
             'Return on Equity (%) (ROE)': info.get('returnOnEquity') * 100,
             'Diluted EPS (ttm) (EPS)': info.get('trailingEps'),
-            'Forward Annual Dividend Rate (DPS)': info.get('dividendRate')
+            'Forward Annual Dividend Rate (DPS)': info.get('dividendRate'),
+            'Current Price (Hrg)': current_price
         }
         data[symbol] = stock_data
     return data
@@ -22,7 +27,9 @@ def main():
     st.title('Yahoo Finance Statistics Scraper')
 
     # Input form untuk simbol saham
-    symbols = st.text_area('Masukkan simbol saham (pisahkan dengan koma)', 'GGRM.JK,MPMX.JK')
+    symbols = st.text_area('Masukkan simbol saham (pisahkan dengan koma)', 'GGRM.JK')
+
+    modal_rupiah = st.number_input("Masukkan modal dalam Rupiah", step=1000000, format="%d")
 
     if st.button('Ambil Data'):
         try:
@@ -31,17 +38,18 @@ def main():
 
             # Buat DataFrame dari data saham
             df = pd.DataFrame(stocks_data).T
-            df.reset_index(inplace=True)
-            df.rename(columns={'index': 'Symbol'}, inplace=True)
 
-            # Tampilkan data yang diminta
+            # Hitung jumlah saham yang dapat dibeli dengan modal yang dimasukkan
+            df['Jumlah Saham'] = modal_rupiah / df['Current Price (Hrg)'].fillna(0)
+            df['Jumlah Saham'] = df['Jumlah Saham'].apply(lambda x: round(x, 2))  # Memformat jumlah saham menjadi dua angka di belakang koma
+
+            # Hitung dividen dan total nilai yang dihasilkan dari dividen
+            df['Dividen (Hasil)'] = df['Jumlah Saham'] * df['Forward Annual Dividend Rate (DPS)']
+            total_dividen = df['Dividen (Hasil)'].sum()
+
+            # Tampilkan hasil perhitungan
             st.subheader('Data Statistik Terbaru')
             st.dataframe(df)
-
-            # Unduh data ke dalam file Excel
-            if st.button('Unduh Data Excel'):
-                excel_file = df.to_excel('stock_data.xlsx', index=False)
-                st.success('Data berhasil diunduh ke dalam file Excel: [stock_data.xlsx](stock_data.xlsx)')
         except Exception as e:
             st.error(f"Terjadi kesalahan: {str(e)}")
 
