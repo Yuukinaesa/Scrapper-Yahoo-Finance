77c575887c7a06333896116a60ca1616320a67a5
Arfan Hidayat Priyantono <arfan.hidayat.priyantono@gmail.com>
2024-06-03 21:02:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ def fetch_stock_data(symbols):
     for symbol in symbols:
         stock = yf.Ticker(symbol)
         info = stock.info
-        current_price = info.get('regularMarketPrice')
-        if current_price is None:
-            current_price = info.get('regularMarketPreviousClose')  # Gunakan harga tutup sebelumnya jika harga saat ini tidak tersedia
+        current_price = info.get('regularMarketPrice', info.get('regularMarketPreviousClose'))
         stock_data = {
             'Symbol': symbol,
             'Price/Book (PBVR)': info.get('priceToBook'),
@@ -26,9 +24,7 @@ def fetch_stock_data(symbols):
 def main():
     st.title('Yahoo Finance Statistics Scraper')
 
-    # Input form untuk simbol saham
     symbols = st.text_area('Masukkan simbol saham (pisahkan dengan koma)', 'MPMX.JK,GGRM.JK')
-
     modal_rupiah = st.number_input("Masukkan modal dalam Rupiah", step=1000000, format="%d")
 
     if st.button('Ambil Data'):
@@ -36,18 +32,10 @@ def main():
             symbols_list = [symbol.strip().upper() for symbol in symbols.split(',')]
             stocks_data = fetch_stock_data(symbols_list)
 
-            # Buat DataFrame dari data saham
             df = pd.DataFrame(stocks_data).T
-
-            # Hitung jumlah saham yang dapat dibeli dengan modal yang dimasukkan
             df['Jumlah Saham'] = modal_rupiah / df['Current Price (Hrg)'].fillna(0)
-            df['Jumlah Saham'] = df['Jumlah Saham'].apply(lambda x: round(x, 2))  # Memformat jumlah saham menjadi dua angka di belakang koma
-
-            # Hitung dividen dan total nilai yang dihasilkan dari dividen
+            df['Jumlah Saham'] = df['Jumlah Saham'].apply(lambda x: round(x, 2))
             df['Dividen (Hasil)'] = df['Jumlah Saham'] * df['Forward Annual Dividend Rate (DPS)']
-            total_dividen = df['Dividen (Hasil)'].sum()
-
-            # Tampilkan hasil perhitungan
             st.subheader('Data Statistik Terbaru')
             st.dataframe(df)
         except Exception as e:
